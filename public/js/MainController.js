184bdc8d2541d35f817617ae64f325551bcb13b9
Christine Yi <christineyi@Christines-MacBook-Air.local>
2016-05-04 20:14:16 -0400

    modifying MainController

--- public/js/MainController.js
+++ public/js/MainController.js
@@ -1,3 +1,7 @@
-var app = angular.module('main', []);
+var app = angular.module('main', ['ngRoute']);
 
 console.log("I WORK MAIN CONTROLLER");
+
+app.controller('MainController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http){
+	this.greeting = "HELLO WORLD";
+}])
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,19 @@
-var app = angular.module('resumeApp', ['main']);
+var app = angular.module('resumeApp', ['ngRoute', 'main']);
 
 console.log("I work");
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	// tell angular to use push state
+    $locationProvider.html5Mode({ enabled: true }); 
+    $routeProvider.
+        when('/', {
+            templateUrl: 'partials/main.html',
+            controller: 'MainController',
+            controllerAs: 'MainCtrl'
+        }).
+        when('/login', {
+            templateUrl: 'partials/login.html',
+            controller: 'FormController',
+            controllerAs: 'FormCtrl'
+        })
+}]);
